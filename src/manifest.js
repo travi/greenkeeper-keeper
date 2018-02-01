b8b3404b62d864419645ffebd2685142a30b6139
Matt Travi <programmer@travi.org>
2018-02-01 08:29:25 -0600

    upgraded to greenkeeper-keeper 2.0.0-beta channel
    this also upgrades the server to v17

--- src/manifest.js
+++ src/manifest.js
@@ -1,40 +1,40 @@
 require('dotenv-safe').load();
 
 export default {
-  connections: [{
-    port: process.env.PORT
-  }],
-  registrations: [
-    {
-      plugin: {
-        register: 'good',
-        options: {
-          reporters: {
-            console: [
-              {
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{log: '*', request: '*', response: '*', error: '*'}]
-              },
-              {module: 'good-console'},
-              'stdout'
-            ]
+  server: {port: process.env.PORT},
+  register: {
+    plugins: [
+      {
+        plugin: {
+          register: 'good',
+          options: {
+            reporters: {
+              console: [
+                {
+                  module: 'good-squeeze',
+                  name: 'Squeeze',
+                  args: [{log: '*', request: '*', response: '*', error: '*'}]
+                },
+                {module: 'good-console'},
+                'stdout'
+              ]
+            }
           }
         }
-      }
-    },
-    {
-      plugin: {
-        register: 'hapi-greenkeeper-keeper',
-        options: {
-          github: {
-            token: process.env.GITHUB_TOKEN
-          },
-          acceptAction: 'rebase'
+      },
+      {
+        plugin: {
+          register: 'hapi-greenkeeper-keeper',
+          options: {
+            github: {
+              token: process.env.GITHUB_TOKEN
+            },
+            acceptAction: 'rebase'
+          }
         }
-      }
-    },
-    {plugin: 'hapi-github-webhooks'},
-    {plugin: './auth'}
-  ]
+      },
+      {plugin: 'hapi-github-webhooks'},
+      {plugin: './auth'}
+    ]
+  }
 };
