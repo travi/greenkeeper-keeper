76bb87683a2140732a9af27109ed45a750e5cac5
Matt Travi <programmer@travi.org>
2018-02-02 00:07:14 -0600

    WIP(hapi-17): got good and hapi-greenkeeper-keeper registered following hapi-17 conventions

--- src/manifest.js
+++ src/manifest.js
@@ -5,32 +5,26 @@ export default {
   register: {
     plugins: [
       {
-        plugin: {
-          register: 'good',
-          options: {
-            reporters: {
-              console: [
-                {
-                  module: 'good-squeeze',
-                  name: 'Squeeze',
-                  args: [{log: '*', request: '*', response: '*', error: '*'}]
-                },
-                {module: 'good-console'},
-                'stdout'
-              ]
-            }
+        plugin: 'good',
+        options: {
+          reporters: {
+            console: [
+              {
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{log: '*', request: '*', response: '*', error: '*'}]
+              },
+              {module: 'good-console'},
+              'stdout'
+            ]
           }
         }
       },
       {
-        plugin: {
-          register: 'hapi-greenkeeper-keeper',
-          options: {
-            github: {
-              token: process.env.GITHUB_TOKEN
-            },
-            acceptAction: 'rebase'
-          }
+        plugin: 'hapi-greenkeeper-keeper',
+        options: {
+          github: {token: process.env.GITHUB_TOKEN},
+          acceptAction: 'rebase'
         }
       },
       {plugin: 'hapi-github-webhooks'},
